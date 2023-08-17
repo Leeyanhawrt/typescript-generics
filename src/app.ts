3bb879abd2f5d456724a2c2a39300a31a65870c0
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-17 14:00:58 -0400

    Generic Functions dynamically set and returned

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,12 @@ promise.then((data) => {
   data.split(" ");
 });
 
+// T and U are placeholders are the values passed in when calling the function
 function merge<T, U>(objA: T, objB: U) {
   return { ...objA, ...objB };
 }
 
+// In this instance, we are passing {name: "Ellissa"} to T and { occupation: "Doctor", partner: "Leeyan" } to U. We are passing the types dynamically when called and TS will infer the result type
 const mergedObj = merge(
   { name: "Ellissa" },
   { occupation: "Doctor", partner: "Leeyan" }
